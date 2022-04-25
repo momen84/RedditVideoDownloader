c645b713cccfc29f903882cbfe52119ca5388b53
momen84 <momen84@hotmail.com>
2022-04-26 02:10:39 +0400

    Update dash_processors_0_2_2.py

--- dash_processors_0_2_2.py
+++ dash_processors_0_2_2.py
@@ -28,51 +28,11 @@ def check_cross_post(json_script,identifier):
 	except AttributeError:
 		return identifier
 
-# def check_post_exists(json_script,identifier):
-# 	removed_flag=json.loads(json_script).get('posts').get('models').get('t3_'+identifier).get('removedByCategory')
-# 	return removed_flag
-	
-# def get_post_title(json_script,identifier):
-# 	post_title=json.loads(json_script).get('posts').get('models').get('t3_'+identifier).get('title')
-# 	return post_title
 
 def get_dashurl(json_script,identifier):
 	
 	identifier=check_cross_post(json_script,identifier)
-	# print(identifier)
-
-	# print(get_post_title(json_script,identifier))
-	# print(check_post_exists(json_script,identifier))
-
-	# media_type=json.loads(json_script).get('posts').get('models').get('t3_'+identifier).keys()#.get('media')#.get('type')
 	
-
-	# try:		
-	# 	dash_file_url=json.loads(json_script).get('posts').get('models').get('t3_'+identifier).get('media').get('dashUrl')
-	# 	dash_file_url,base_url,dash_file=get_dash_file(dash_file_url)
-
-	# 	return dash_file_url,base_url,dash_file
-	# except NoneError:
-	# 	dash_file_url=json.loads(json_script).get('posts').get('models').get('t3_'+identifier).get('media').get('videoPreview').get('dashUrl')
-	# 	dash_file_url,base_url,dash_file=get_dash_file(dash_file_url)
-
-	# 	return dash_file_url,base_url,dash_file
-	
-	# except AttributeError:
-	# 	file_url=json.loads(json_script).get('posts').get('models').get('t3_'+identifier).get('source').get('url')
-	# 	file_url=file_url.replace('.gifv','.mp4')
-	# 	print('Redirecting outside of reddit to {}'.format(file_url))
-	# 	if 'imgur'in file_url:
-	# 		download_files=core.get_size(file_url)
-	# 		return download_files,None,None
-	# 	elif 'gfycat' in file_url:
-	# 		download_files=core.get_outsiders(file_url)
-	# 		return download_files,None,None
-	# 	elif 'redgifs' in file_url:
-	# 		download_files=core.get_outsiders(file_url)
-	# 		return download_files,None,None
-		# else:
-	# 		return 'Error','Error','Error'
 	return 'Error','Error','Error'
 
 
